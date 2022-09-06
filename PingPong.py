1fa5d19c79c838401d6d737928d2f871b42032af
Ardaerd <arderd@outlook.com>
2022-09-06 12:05:32 +0300

    now extra ball is generated after the 5s

--- PingPong.py
+++ PingPong.py
@@ -5,7 +5,7 @@ from Ball import Ball
 from Pen import Pen
 import winsound
 from timeit import default_timer as timer
-        
+
 screen = turtle.Screen()
 screen.title("PingPong")
 screen.bgcolor("black")
@@ -19,29 +19,30 @@ screen.listen()
 balls = []
 
 # Create paddle A
-paddle_a = Paddle(-350,0)
+paddle_a = Paddle(-350, 0)
 
 # Create paddle B
-paddle_b = Paddle(350,0)
+paddle_b = Paddle(350, 0)
 
 # Create Ball
-ball_1 = Ball(0,0)
+ball_1 = Ball(0, 0)
 balls.append(ball_1)
 
 # Create the scoreBoard
 pen = Pen()
 
 # For Paddle which is going up
-screen.onkeypress(paddle_a.paddle_up,"w")
-screen.onkeypress(paddle_b.paddle_up,"Up")
+screen.onkeypress(paddle_a.paddle_up, "w")
+screen.onkeypress(paddle_b.paddle_up, "Up")
 
 
 # For Paddle which is going down
-screen.onkeypress(paddle_a.paddle_down,"s")
-screen.onkeypress(paddle_b.paddle_down,"Down")
+screen.onkeypress(paddle_a.paddle_down, "s")
+screen.onkeypress(paddle_b.paddle_down, "Down")
+
 
 def collision(limit):
-    
+
     for ball in balls:
       # For paddle_b
         if (ball.ball.xcor() > limit and ball.ball.xcor() < limit + 10) and (ball.ball.ycor() < paddle_b.paddle.ycor() + 45 and ball.ball.ycor() > paddle_b.paddle.ycor() - 45):
@@ -49,9 +50,10 @@ def collision(limit):
             ball.ball.dx *= -1
 
         # For paddle_a
-        if (ball.ball.xcor() < -limit and ball.ball.xcor() > -limit -10) and (ball.ball.ycor() < paddle_a.paddle.ycor() + 45 and ball.ball.ycor() > paddle_a.paddle.ycor() - 45):
+        if (ball.ball.xcor() < -limit and ball.ball.xcor() > -limit - 10) and (ball.ball.ycor() < paddle_a.paddle.ycor() + 45 and ball.ball.ycor() > paddle_a.paddle.ycor() - 45):
             ball.ball.setx(-limit)
             ball.ball.dx *= -1
+
 
 start = timer()
 
@@ -59,17 +61,16 @@ start = timer()
 while True:
     time.sleep(1 / 60)
     screen.update()
-    
+
     for ball in balls:
         # Move the ball
         ball.setCoordinates()
-    
+
         # Border Checking
         ball.checkBorder_Y(290)
         ball.checkBorder_Y(-290)
         ball.checkBorder_X(390)
 
-    
         # Updating the score board
         if ball.ball.xcor() <= -390:
             pen.score_b += 1
@@ -81,7 +82,7 @@ while True:
                     ball.clearBall()
                 balls.remove(ball)
             start = timer()
-        
+
         elif ball.ball.xcor() >= 390:
             pen.score_a += 1
             pen.pen.clear()
@@ -94,13 +95,12 @@ while True:
                 balls.remove(ball)
             start = timer()
 
-
-    # Paddle and ball collision 
+    # Paddle and ball collision
     collision(340)
-    
+
     end = timer()
-    
+
     if (end - start) > 5:
         start = timer()
-        new_ball = Ball(0,0)
+        new_ball = Ball(0, 0)
         balls.append(new_ball)
